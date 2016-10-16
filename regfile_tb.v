095167892484d635835530c0905eee8cf15455f4
arik1990 <amikkelsen@hotmail.ca>
2016-10-16 15:39:33 -0700

    Update regfile_tb.v

--- regfile_tb.v
+++ regfile_tb.v
@@ -1,35 +1,36 @@
 module register_file_tb ;
 
-reg [2:0] writenum, readnum;
-reg clk,write;
-reg [15:0] data_in; 
+    reg [2:0] writenum, readnum;
+    reg clk,write;
+    reg [15:0] data_in; 
  
-wire [15:0] data_out;
+    wire [15:0] data_out;
 
-reg_file DUT (.writenum(writenum), .write(write), .data_in(data_in), .clk(clk), .readnum(readnum), .data_out(data_out));
+        reg_file DUT (.writenum(writenum), .write(write), .data_in(data_in), .clk(clk), .readnum(readnum), .data_out(data_out));
 
 
-initial begin
+    initial begin
 
-clk=1; #5 clk=0;
-forever 
-begin
-$display("writenum = %b, write = %b, data_in = %b, clk = %b, readnum = %b, data_out = %b", writenum, write, data_in, clk, readnum, data_out);
-#5 clk = 1; #5 clk = 0;
-end
-end
+        clk=1; #5 clk=0;
+        forever 
+        begin
+        $display("writenum = %b, write = %b, data_in = %b, clk = %b, readnum = %b, data_out = %b", writenum, write, data_in, clk, readnum, data_out);
+        #5 clk = 1; #5 clk = 0;
+        end
+        end
 
-initial begin
+    initial begin
 
-writenum = 3'b000; readnum = 3'b000; write = 0; data_in = {16{1'b0}};
-#15 write = 1;
-#15 write = 0;
-#30
-writenum = 3'b001; write = 1; data_in = {16{1'b1}};
-#15 write = 0; readnum = 3'b001;
-#50
-$stop;
-end 
+
+        writenum = 3'b000; readnum = 3'b000; write = 0; data_in = {16{1'b0}};
+        #15 write = 1;
+        #15 write = 0;
+        #30
+        writenum = 3'b001; write = 1; data_in = {16{1'b1}};
+        #15 write = 0; readnum = 3'b001;
+        #50
+        $stop;
+    end 
 endmodule
 
 
